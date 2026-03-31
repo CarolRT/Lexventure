37fe5fcced7bf897a1da71f44e68a044cc028848
Carolina <carolinarodenasformacion@outlook.com>
2026-03-31 20:20:50 +0200

    feat: implement MVP basic interaction

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log('Esto es una prueba')
+//console.log('Esto es una prueba')
+  const board = document.getElementById('game-board');
+  
+  board.addEventListener('click', (event) => {
+    const clickCard = event.target.closest('.card');
+    
+    if (clickCard) {
+        clickCard.classList.toggle('is-flipped'); //Permite que al hacer click gire
+    }
+  });
